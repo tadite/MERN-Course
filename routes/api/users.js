c4e2fde1382cb14da5cda0e3a24941ce25eae0a8
Nazarov Andrey <nazarov.a.dev@gmail.com>
2018-10-09 21:48:04 +0300

    Added Bearer authorization

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,100 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
+
+const User = require("../../models/User");
 
 // @route GET /api/users/test
 // @desc Test for users
 // @access public
-router.get('/test', (req,res)=>res.json({msg:"Users is OK"}));
+router.get("/test", (req, res) => res.json({ msg: "Users is OK" }));
+
+// @route POST /api/users/register
+// @desc Test for users
+// @access public
+router.post("/register", (req, res) => {
+	User.findOne({ email: req.body.email }).then(user => {
+		if (user) {
+			return res.status(400).json({ email: "Email already exists" });
+		} else {
+			const avatar = gravatar.url(req.body.email, {
+				s: "200",
+				r: "pg",
+				d: "mm"
+			});
+
+			const newUser = new User({
+				name: req.body.name,
+				email: req.body.email,
+				password: req.body.password,
+				avatar
+			});
+
+			bcrypt.genSalt(10, (err, salt) => {
+				if (err) throw err;
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if (err) throw err;
+					newUser.password = hash;
+					newUser
+						.save()
+						.then(user => res.json(user))
+						.catch(err => console.log(err));
+				});
+			});
+		}
+	});
+});
+
+// @route POST /api/users/login
+// @desc Login for users
+// @access public
+router.post("/login", (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	User.findOne({ email }).then(user => {
+		if (!user) {
+			return res.status(404).json({ msg: "User not found" });
+		}
+		bcrypt.compare(password, user.password).then(isMatch => {
+			if (isMatch) {
+				const payload = { id: user.id, name: user.name, avatar: user.avatar };
+				jwt.sign(
+					payload,
+					keys.secretOrKey,
+					{ expiresIn: 3600 },
+					(err, token) => {
+						if (err) throw err;
+						res.json({
+							success: true,
+							token: "Bearer " + token
+						});
+					}
+				);
+			} else {
+				return res.status(400).json({ msg: "Incorrect password" });
+			}
+		});
+	});
+});
+
+// @route POST /api/users/current
+// @desc Current user
+// @access private
+router.get(
+	"/current",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		res.json({
+			id: req.user.id,
+			name: req.user.name,
+			email: req.user.email
+		});
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users=require('./routes/api/users');
-const profile=require('./routes/api/profile');
-const posts=require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body parser
+app.use(bodyParser.urlencoded(false));
+app.use(bodyParser.json());
 
 //MongoDb
 const dbUri = require("./config/keys").mongoUri;
 
 mongoose
-  .connect(dbUri)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+	.connect(dbUri)
+	.then(() => console.log("MongoDB connected"))
+	.catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+app.use(passport.initialize());
 
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
-
