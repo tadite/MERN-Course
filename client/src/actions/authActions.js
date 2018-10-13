8ce07a7d1a5a70cb3e418f65acdeaf0a02c863f3
Nazarov Andrey <nazarov.a.dev@gmail.com>
2018-10-13 23:53:09 +0300

    Added login and token usage with localStorage, axios defaults and redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 //Register
 export const registerUser = (userData, history) => dispatch => {
@@ -7,4 +10,25 @@ export const registerUser = (userData, history) => dispatch => {
 		.post("/api/users/register", userData)
 		.then(res => history.push("/login"))
 		.catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+//Login
+export const loginUser = userData => dispatch => {
+	axios
+		.post("/api/users/login", userData)
+		.then(res => {
+			const { token } = res.data;
+			localStorage.setItem("jwtToken", token);
+			setAuthToken(token);
+			const decoded = jwt_decode(token);
+			dispatch(setCurrentUser(decoded));
+		})
+		.catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+export const setCurrentUser = decoded => {
+	return {
+		type: SET_CURRENT_USER,
+		payload: decoded
+	};
 };
