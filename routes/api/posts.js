6c85aec0242f4e0f63f4cb90436d86122e5469b2
Nazarov Andrey <nazarov.a.dev@gmail.com>
2018-10-13 12:26:28 +0300

    Added post model and create post api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+
+const validatePostnInput = require("../../validation/post");
+
+const Post = require("../../models/Post");
+const User = require("../../models/User");
 
 // @route GET /api/posts/test
 // @desc Test for posts
 // @access public
-router.get('/test', (req,res)=>res.json({msg:"Posts is OK"}));
+router.get("/test", (req, res) => res.json({ msg: "Posts is OK" }));
+
+// @route POST /api/posts
+// @desc Create post
+// @access public
+router.post(
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		const { errors, isValid } = validatePostnInput(req.body);
+
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
+
+		const newPost = new Post({
+			text: req.body.text,
+			name: req.body.name,
+			avatar: req.user.avatar,
+			user: req.user.id
+		});
+
+		newPost.save().then(post => res.json(post));
+	}
+);
 
 module.exports = router;
---
+++ validation/post.js
@@ -0,0 +1,17 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostnInput(data) {
+	let errors = {};
+
+	if (!data.text || Validator.isEmpty(data.text)) {
+		errors.text = "Text is required";
+	} else if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+		errors.text = "Text length must be between 10 and 300 symbols";
+	}
+
+	return {
+		errors,
+		isValid: isEmpty(errors)
+	};
+};
