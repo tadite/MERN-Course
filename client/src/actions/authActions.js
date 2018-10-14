55bfc6ffa4bbbcc5143d0bf0cf3a5e56db497989
Nazarov Andrey <nazarov.a.dev@gmail.com>
2018-10-14 21:08:02 +0300

    Implemeted logout logic

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -32,3 +32,9 @@ export const setCurrentUser = decoded => {
 		payload: decoded
 	};
 };
+
+export const logoutUser = () => dispatch => {
+	localStorage.removeItem("jwtToken");
+	setAuthToken(false);
+	dispatch(setCurrentUser({}));
+};
